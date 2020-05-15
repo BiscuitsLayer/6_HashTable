1e2421431bbf9ed2af45840f44e8c19bd71d224c
Sergey <vinogradov.so@phystech.edu>
2020-05-15 19:46:15 +0300

    Improved a bit

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ const int SERVICE = 118;  //Служебная константа (для пос
 const int HT_SIZE = 101;
 const int BUF_EXTRA_SIZE = 1;
 const char *FILEPATH = "inputs/input.txt";
+const char *POISON = "POISON";  //Ядовитое значение
 
 //Тип содержимого списка
 typedef char * Elem_t;
@@ -35,9 +36,6 @@ typedef struct {
 	size_t *sizes = nullptr;
 } HT_t;
 
-char *buf_to_free = nullptr;
-size_t words_num = 0;
-
 //Операции со списком
 List_t ListInit (size_t size);                                      //Конструктор
 void ListDestruct (List_t *list);                                   //Деструктор
@@ -56,32 +54,19 @@ void ListSort (List_t *list);                                       //Перес
 
 //Операции с таблицей
 HT_t HTInit (size_t size);
-void HTDestruct (HT_t *ht);
+void HTDestruct (HT_t *ht, char *buf);
+void HTFill (HT_t *ht, char *buf, unsigned long int (*HF) (char *str), size_t words_num);
 void HTInsert (HT_t *ht, unsigned long int (*HF) (char *str), char *str);
 bool HTSearch (HT_t *ht, char *str);
-/*
-unsigned long int EQ1 (char *str);
-unsigned long int MURMUR (char *str);
-unsigned long int FirstLetter (char *str);
-unsigned long int Len (char *str);
-unsigned long int Sum (char *str);
-unsigned long int BesouIdentity (char *str);
-unsigned long int JSHash (char *str);
-unsigned long int RSHash (char *str);
-unsigned long int RolHash (char *str);
-unsigned long int HashLy (char *str);
-unsigned long int GNU_HASH (char *str);
-*/
+
 unsigned long int CRC32(char *str);
 
 unsigned long crc_table_opt1[256] = { 0, 1996959894, 3993919788, 2567524794, 124634137, 1886057615, 3915621685, 2657392035, 249268274, 2044508324, 3772115230, 2547177864, 162941995, 2125561021, 3887607047, 2428444049, 498536548, 1789927666, 4089016648, 2227061214, 450548861, 1843258603, 4107580753, 2211677639, 325883990, 1684777152, 4251122042, 2321926636, 335633487, 1661365465, 4195302755, 2366115317, 997073096, 1281953886, 3579855332, 2724688242, 1006888145, 1258607687, 3524101629, 2768942443, 901097722, 1119000684, 3686517206, 2898065728, 853044451, 1172266101, 3705015759, 2882616665, 651767980, 1373503546, 3369554304, 3218104598, 565507253, 1454621731, 3485111705, 3099436303, 671266974, 1594198024, 3322730930, 2970347812, 795835527, 1483230225, 3244367275, 3060149565, 1994146192, 31158534, 2563907772, 4023717930, 1907459465, 112637215, 2680153253, 3904427059, 2013776290, 251722036, 2517215374, 3775830040, 2137656763, 141376813, 2439277719, 3865271297, 1802195444, 476864866, 2238001368, 4066508878, 1812370925, 453092731, 2181625025, 4111451223, 1706088902, 314042704, 2344532202, 4240017532, 1658658271, 366619977, 2362670323, 4224994405, 1303535960, 984961486, 2747007092, 3569037538, 1256170817, 1037604311, 2765210733, 3554079995, 1131014506, 879679996, 2909243462, 3663771856, 1141124467, 855842277, 2852801631, 3708648649, 1342533948, 654459306, 3188396048, 3373015174, 1466479909, 544179635, 3110523913, 3462522015, 1591671054, 702138776, 2966460450, 3352799412, 1504918807, 783551873, 3082640443, 3233442989, 3988292384, 2596254646, 62317068, 1957810842, 3939845945, 2647816111, 81470997, 1943803523, 3814918930, 2489596804, 225274430, 2053790376, 3826175755, 2466906013, 167816743, 2097651377, 4027552580, 2265490386, 503444072, 1762050814, 4150417245, 2154129355, 426522225, 1852507879, 4275313526, 2312317920, 282753626, 1742555852, 4189708143, 2394877945, 397917763, 1622183637, 3604390888, 2714866558, 953729732, 1340076626, 3518719985, 2797360999, 1068828381, 1219638859, 3624741850, 2936675148, 906185462, 1090812512, 3747672003, 2825379669, 829329135, 1181335161, 3412177804, 3160834842, 628085408, 1382605366, 3423369109, 3138078467, 570562233, 1426400815, 3317316542, 2998733608, 733239954, 1555261956, 3268935591, 3050360625, 752459403, 1541320221, 2607071920, 3965973030, 1969922972, 40735498, 2617837225, 3943577151, 1913087877, 83908371, 2512341634, 3803740692, 2075208622, 213261112, 2463272603, 3855990285, 2094854071, 198958881, 2262029012, 4057260610, 1759359992, 534414190, 2176718541, 4139329115, 1873836001, 414664567, 2282248934, 4279200368, 1711684554, 285281116, 2405801727, 4167216745, 1634467795, 376229701, 2685067896, 3608007406, 1308918612, 956543938, 2808555105, 3495958263, 1231636301, 1047427035, 2932959818, 3654703836, 1088359270, 936918000, 2847714899, 3736837829, 1202900863, 817233897, 3183342108, 3401237130, 1404277552, 615818150, 3134207493, 3453421203, 1423857449, 601450431, 3009837614, 3294710456, 1567103746, 711928724, 3020668471, 3272380065, 1510334235, 755167117 };
 void CRC32_tablefill();
 unsigned long int CRC32_opt1(char *str);
 
-void ReadData (HT_t *ht, FILE *readfile, unsigned long int (*HF) (char *str));
-char *GetWordsNum (FILE *readfile, struct stat file_info);
-void PrintStart (FILE *writefile);
-void PrintData (HT_t *ht, FILE *writefile, char *name);
+char *ReadData (HT_t *ht, size_t *words_num, FILE *readfile, unsigned long int (*HF) (char *str));
+size_t BufHandle (char *buf, struct stat file_info);
 
 int main () {
     HT_t ht = HTInit (HT_SIZE);
@@ -91,47 +76,32 @@ int main () {
 		printf ("Error opening file\n");
 		return 0;
 	}
-    /*
-    ReadData (&ht, readfile, JSHash);
-    ReadData (&ht, readfile, RSHash);
-    ReadData (&ht, readfile, BesouIdentity);
-    ReadData (&ht, readfile, Sum);
-    ReadData (&ht, readfile, Len);
-    ReadData (&ht, readfile, FirstLetter);
-    ReadData (&ht, readfile, MURMUR);
-    ReadData (&ht, readfile, EQ1);
-    ReadData (&ht, readfile, RolHash);
-    ReadData (&ht, readfile, HashLy);
-    */
-    ReadData (&ht, readfile, CRC32_opt1);
-    fclose (readfile);
-	printf ("Words number: %d\n", words_num);
-	/*
-    FILE *writefile = fopen ("../output.csv", "w");
-    PrintStart (writefile);
-    PrintData (&ht, writefile, "JSHash");
-    fclose (writefile);
-    */
 
-   size_t counter = 0;
+	size_t words_num = 0;
+    char *buf = ReadData (&ht, &words_num, readfile, CRC32_opt1);
+	printf ("read %zu words\n", words_num);
+    fclose (readfile);
+
+	//printf ("Words number: %d\n", words_num);
+	HTFill (&ht, buf, CRC32_opt1, words_num);
+	printf ("filled successfully\n");
+	/*
+   	size_t counter = 0;
     for (size_t i = 0; i < HT_SIZE; ++i) {
     	for (size_t j = 0; j < ht.sizes[i]; ++j) {
 			if (HTSearch (&ht, ht.lists[i].items[j].data) == true)
 				++counter;
     	}
     }
-
-	printf ("%d\n", counter);
-    HTDestruct (&ht);
+	*/
+    HTDestruct (&ht, buf);
     return 0;
 }
-
-const char *POISON = "POISON";  //Ядовитое значение
 
 List_t ListInit (size_t size) {
 	List_t list = {};
 	//Выделение памяти для содержимого списка
-	list.items = (List_it *) calloc (size, sizeof(List_t));
+	list.items = (List_it *) calloc (size, sizeof(List_it));
 	assert (list.items);
 
 	list.size = 0;
@@ -163,6 +133,7 @@ void ListDestruct (List_t *list) {
 
 void ListOK (List_t *list) {
 	return;
+	/*
 	if (list->free == 0)
 		printf ("WARNING! List overflow!\n");
 
@@ -190,6 +161,7 @@ void ListOK (List_t *list) {
 
 	//Дамп списка
 	//ListDump (list);
+	*/
 }
 
 size_t ListValSearch (List_t *list, Elem_t val) {
@@ -199,6 +171,7 @@ size_t ListValSearch (List_t *list, Elem_t val) {
 		
 		char *temp = list->items[idx].data;
 		char res = 0;
+		/*
 		asm(
 			"	.intel_syntax_noprefix\n\t"
 			"	mov rsi, [v]\n\t"
@@ -251,9 +224,9 @@ void ListPushBefore (List_t *list, Elem_t val_s, Elem_t val_i) {
 	list->free = list->items[cur].next;
 	++list->size; //Увеличиваем размер списка
 	//Поиск следующего за данным элемента
-	size_t idx_after = ListValSearch(list, val_s);
+	size_t idx_after = ListValSearch (list, val_s);
 	if (!idx_after) {
-		printf("Element %d not found\n", val_s);
+		printf("Element %s not found\n", val_s);
 		exit(1);
 	}
 	//Если вставили в самое начало
@@ -279,7 +252,7 @@ void ListPushAfter (List_t *list, Elem_t val_s, Elem_t val_i) {
 	//Поиск предыдущего элемента
 	size_t idx_before = ListValSearch(list, val_s);
 	if (!idx_before) {
-		printf("Element %d not found\n", val_s);
+		printf("Element %s not found\n", val_s);
 		exit(1);
 	}
 	//Если вставили в самый конец
@@ -378,119 +351,10 @@ void ListPrint(List_t *list) {
 	printf ("\n");
 }
 
-void ListDump (List_t *list) {
-	//Дамп в консоль
-	printf ("-------");
-
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("----------");
-	printf ("\n");
-
-	printf ("HEAD: %zu, TAIL: %zu, SIZE: %zu, FREE: %zu\n", list->head, list->tail, list->size, list->free);
-
-	printf ("-------");
-
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("----------");
-	printf ("\n");
-
-	printf (" idx: ");
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("%10zu", idx);
-	printf ("\n");
-
-	printf ("-------");
-
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("----------");
-	printf ("\n");
-
-	printf ("prev: ");
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("%10zu", list->items[idx].prev);
-	printf ("\n");
-
-	printf ("data: ");
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("%10s", list->items[idx].data);
-	printf ("\n");
-
-	printf ("next: ");
-	for (size_t idx = 0; idx < LIST_SIZE; ++idx)
-		printf ("%10zu", list->items[idx].next);
-	printf ("\n");
-
-	printf ("\n");
-	//Окончание дампа в консоль
-
-	//Запись в dot файл (инициализация графа)
-	FILE *Dot = fopen ("../temp.dot", "w");
-	fprintf (Dot, "digraph {\nnode [shape=\"box\"]\n"
-	              "edge [style=\"dashed\" dir=\"both\"]\n"
-	              "dirtype = \"both\"\n"
-	              "rankdir=\"LR\"\n");
-
-	//Запись всех элементов списка в dot файл
-	for (size_t idx = list->head; idx != 0; idx = list->items[idx].next) {
-		fprintf (Dot, "\telem_%zu [label=<<TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"3\">\n"
-		              "\t\t<TR><TD COLSPAN=\"2\" BGCOLOR=\"white\"> idx: %zu </TD></TR>\n"
-		              "\t\t<TR><TD COLSPAN=\"2\" BGCOLOR=\"white\"> data: %s </TD></TR>\n"
-		              "\t\t<TR><TD BGCOLOR=\"salmon\" PORT=\"prev\"> prev: %d </TD>\n"
-		              "\t\t<TD BGCOLOR=\"lawngreen\" PORT=\"next\"> next: %d </TD></TR>\n"
-		              "\t</TABLE>>]\n", idx, idx, list->items[idx].data,
-				list->items[idx].prev == 0 ? -1 : list->items[idx].prev,
-				list->items[idx].next == 0 ? -1 : list->items[idx].next);
-	}
-	//Если список непуст
-	if (list->head != 0)
-		fprintf (Dot, "\"data\" -> elem_%zu\n", list->head);
-	else
-		fprintf (Dot, "\"data\" -> \"empty\"\n");
-	for (size_t idx = list->head; list->items[idx].next != 0; idx = list->items[idx].next)
-		fprintf (Dot, "elem_%zu -> elem_%zu\n", idx, list->items[idx].next);
-
-	//Запись свободных ячеек в dot файл
-	for (size_t idx = list->free; idx != 0; idx = list->items[idx].next) {
-		fprintf (Dot, "\telem_free_%zu [label=<<TABLE BORDER=\"0\" CELLBORDER=\"1\" CELLSPACING=\"3\">\n"
-		              "\t\t<TR><TD COLSPAN=\"2\" BGCOLOR=\"aquamarine\"> idx: %zu </TD></TR>\n"
-		              "\t\t<TR><TD COLSPAN=\"2\" BGCOLOR=\"aquamarine\"> data: %s </TD></TR>\n"
-		              "\t\t<TR><TD BGCOLOR=\"salmon\" PORT=\"prev\"> prev: %d </TD>\n"
-		              "\t\t<TD BGCOLOR=\"lawngreen\" PORT=\"next\"> next: %d </TD></TR>\n"
-		              "\t</TABLE>>]\n", idx, idx, list->items[idx].data,
-				list->items[idx].prev == 0 ? -1 : list->items[idx].prev,
-				list->items[idx].next == 0 ? -1 : list->items[idx].next);
-	}
-	//Если список не заполнен
-	if (list->free != 0)
-		fprintf (Dot, "\"free\" -> elem_free_%zu\n", list->free);
-	else
-		fprintf (Dot, "\"free\" -> \"empty\"\n");
-	for (size_t idx = list->free; list->items[idx].next != 0; idx = list->items[idx].next)
-		fprintf (Dot, "elem_free_%zu -> elem_free_%zu\n", idx, list->items[idx].next);
-
-	fprintf (Dot, "}\n");
-	fclose (Dot);
-	//Окончание ввода в dot файл
-
-	//Создание gif изображения
-	char *command = (char *) calloc (NUM_SIZE + SERVICE, sizeof (char));
-	sprintf (command, "dot -Tpng /home/biscuitslayer/CLionProjects/6_HashTable/temp.dot "
-	                  "-o/home/biscuitslayer/CLionProjects/6_HashTable/images/temp%zu.png", list->dump_count);
-
-	//Если программа работает заново, папка с изображениями очищается
-	if (list->dump_count == 1)
-		system ("rm /home/biscuitslayer/CLionProjects/6_HashTable/images/*");
-	system (command);
-
-	//Освобождение памяти по команду и увеличение счётчика дампа
-	free (command);
-	++list->dump_count;
-}
-
 void ListSort (List_t *list) {
 	ListOK(list);
 	//Создание массива с отсортированным содержимым
-	Elem_t *sorted_list_data = (Elem_t *) calloc(list->size, sizeof(Elem_t));
+	Elem_t *sorted_list_data = (Elem_t *) calloc (list->size, sizeof(Elem_t));
 
 	//Копирование отсортированного списка в массив
 	for (size_t idx = list->head, sorted_idx = 0; idx != 0; idx = list->items[idx].next, ++sorted_idx)
@@ -524,20 +388,16 @@ HT_t HTInit (size_t size) {
     return ht;
 }
 
-void HTDestruct (HT_t *ht) {
+void HTDestruct (HT_t *ht, char *buf) {
 	for (int i = 0; i < HT_SIZE; ++i)
 		ListDestruct (&ht->lists[i]);
-	free (buf_to_free);
     free (ht->sizes);
     free (ht->lists);
+	free (buf);
 }
 
-void ReadData (HT_t *ht, FILE *readfile, unsigned long int (*HF) (char *str)) {
-    struct stat file_info = {};
-    stat (FILEPATH, &file_info);
-    char *buf = GetWordsNum (readfile, file_info);
-    buf_to_free = buf;
-    for (size_t idx = 0; idx < words_num; ++idx) {
+void HTFill (HT_t *ht, char *buf, unsigned long int (*HF) (char *str), size_t words_num) {
+	for (size_t idx = 0; idx < words_num; ++idx) {
         HTInsert (ht, HF, buf);
         while (*buf != '\0')
             ++buf;
@@ -549,26 +409,39 @@ void ReadData (HT_t *ht, FILE *readfile, unsigned long int (*HF) (char *str)) {
     }
 }
 
-void PrintStart (FILE *writefile) {
-    fprintf (writefile, "LIST_NUM");
-    for (size_t idx = 0; idx < HT_SIZE; ++idx)
-        fprintf (writefile, ",%zu", idx);
-    fprintf (writefile, "\n");
-
+void HTInsert (HT_t *ht, unsigned long int (*HF) (char *str), char *str) {
+    unsigned long int code = HF (str);
+    code %= HT_SIZE;
+    List_t *list = &(ht->lists[code]);
+    if (list->items == nullptr)
+        *list = ListInit (LIST_SIZE);
+    ListPushBack (list, str);
+    ht->sizes[code]++;
 }
 
-void PrintData (HT_t *ht, FILE *writefile, char *name) {
-    fprintf (writefile, "%s", name);
-    for (size_t idx = 0; idx < HT_SIZE; ++idx)
-        fprintf (writefile, ",%d", ht->sizes[idx]);
-    fprintf (writefile, "\n");
+bool HTSearch (HT_t *ht, char *str) {
+	size_t temp = 0, i = 0;
+	while (temp == 0 && i < HT_SIZE) {
+		temp = ListValSearch (&ht->lists[i], str);
+		++i;
+	}
+	return temp != 0;
 }
 
-char *GetWordsNum (FILE *user_input, struct stat file_info) {
-    char *buf = (char *) calloc (file_info.st_size + BUF_EXTRA_SIZE, sizeof (char)); //Буфер из файла ввода пользователя
-    fread (buf, sizeof(char), file_info.st_size, user_input); //Загрузка ввода пользователя в буфер
+char *ReadData (HT_t *ht, size_t *words_num, FILE *readfile, unsigned long int (*HF) (char *str)) {
+    struct stat file_info = {};
+    stat (FILEPATH, &file_info);
 
+	char *buf = (char *) calloc (file_info.st_size + BUF_EXTRA_SIZE, sizeof (char)); //Буфер из файла ввода пользователя
+    fread (buf, sizeof(char), file_info.st_size, readfile); //Загрузка ввода пользователя в буфер
+
+    *words_num = BufHandle (buf, file_info);
+	return buf;
+}
+
+size_t BufHandle (char *buf, struct stat file_info) {
     size_t i = 0; //Индекс в буфере
+	size_t words_num = 0; //Счётчик слов
 
     //Пропуск пробелов до первой команды
     while (isspace (buf[i])) ++i;
@@ -592,163 +465,8 @@ char *GetWordsNum (FILE *user_input, struct stat file_info) {
             ++words_num;
         }
     }
-    return buf;
-}
 
-void HTInsert (HT_t *ht, unsigned long int (*HF) (char *str), char *str) {
-    unsigned long int code = HF (str);
-    code %= HT_SIZE;
-    List_t *list = &(ht->lists[code]);
-    if (list->items == nullptr)
-        *list = ListInit (LIST_SIZE);
-    ListPushBack (list, str);
-    ++ht->sizes[code];
-}
-
-bool HTSearch (HT_t *ht, char *str) {
-	size_t temp = 0, i = 0;
-	while (temp == 0 && i < HT_SIZE) {
-		temp = ListValSearch (&ht->lists[i], str);
-		++i;
-	}
-	return temp != 0;
-}
-
-unsigned long int EQ1 (char *str) {
-    //Возвращает единицу
-    return 1;
-}
-
-unsigned long int FirstLetter (char *str) {
-    //Возвращает ASCII код первого символа
-    return (unsigned long int) tolower (str[0]);
-}
-
-unsigned long int BesouIdentity (char *str) {
-    //Простые числа
-    const int A = 54059, B = 76963, C = 86969, FIRSTH = 37;
-    char *s = str;
-        unsigned h = FIRSTH;
-        while (*s) {
-            h = (h * A) ^ (s[0] * B);
-            s++;
-        }
-        return h % C;
-}
-
-unsigned long int RSHash (char *str) {
-    unsigned int b    = 378551;
-    unsigned int a    = 63689;
-    unsigned int hash = 0;
-
-    for(std::size_t i = 0; i < strlen(str); i++)
-    {
-        hash = hash * a + str[i];
-        a    = a * b;
-    }
-
-    return (hash & 0x7FFFFFFF);
-}
-
-unsigned long int JSHash (char *str) {
-    unsigned int hash = 1315423911;
-    for(size_t i = 0; i < strlen(str); i++)
-        hash ^= ((hash << 5) + str[i] + (hash >> 2));
-    return (hash & 0x7FFFFFFF);
-}
-
-unsigned long int Len (char *str) {
-    //Возвращает длину строки
-    return strlen (str);
-}
-
-unsigned long int HashLy(char *str) {
-    unsigned int hash = 0;
-
-    for(; *str; str++)
-        hash = (hash * 1664525) + (unsigned char)(*str) + 1013904223;
-
-    return hash;
-}
-
-unsigned long int Sum (char *str) {
-    unsigned long int code = 0;
-    char *c = str;
-    while (*c++ != '\0')
-        code += (int) *c;
-    return code;
-}
-
-unsigned long int GNU_HASH (char *str)
-{
-    unsigned long int h = 5381;
-
-    for (unsigned char c = *str; c != '\0'; c = *++str)
-        h = h * 33 + c;
-
-    return h;
-}
-
-unsigned long int MURMUR (char *str) {
-    char *key = str;
-    unsigned int len = strlen (str);
-
-    const unsigned int m = 0x5bd1e995;
-    const unsigned int seed = 0;
-    const int r = 24;
-
-    unsigned int h = seed ^ len;
-
-    const unsigned char * data = (const unsigned char *) key;
-    unsigned int k;
-
-    while (len >= 4) {
-        k  = data [0];
-        k |= data [1] << 8;
-        k |= data [2] << 16;
-        k |= data [3] << 24;
-
-        k *= m;
-        k ^= k >> r;
-        k *= m;
-
-        h *= m;
-        h ^= k;
-
-        data += 4;
-        len -= 4;
-    }
-
-    switch (len) {
-        case 3:
-            h ^= data [2] << 16;
-        case 2:
-            h ^= data [1] << 8;
-        case 1:
-            h ^= data [0];
-            h *= m;
-    };
-
-    h ^= h >> 13;
-    h *= m;
-    h ^= h >> 15;
-
-    return h;
-}
-
-unsigned long int rol (unsigned long int v, unsigned long shift) {
-    unsigned long s =  shift >=  0 ? shift % 32 : - ((-shift) % 32);
-    return (v << s) | (v >> (32 - s) );
-}
-
-unsigned long int RolHash (char *str) {
-    unsigned long int code = 0;
-    char *c = str;
-    while (*c++ != '\0') {
-        code = rol (code, 1);
-        code ^= (unsigned long int) *c;
-    }
-    return code;
+	return words_num;
 }
 
 unsigned long int CRC32 (char *str) {
